49bbb24640de7399e4dac65f8d8b9ef8bb2250d6
Carl Henry Roosipuu <carlhenryr@gmail.com>
2018-09-24 14:16:24 -0400

    Login crash fixed, logout button

--- app/src/main/java/roomies/donationtracker/MainActivity.java
+++ app/src/main/java/roomies/donationtracker/MainActivity.java
@@ -1,8 +1,8 @@
 package roomies.donationtracker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
                     //correct, login
                     Toast.makeText(getApplicationContext(),
                             "Correct! Logging you in...", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(MainActivity.this, InitialScreen.class));
+                    Intent i = new Intent(MainActivity.this, InitialScreen.class);
+                    startActivity(i);
                 } else {
                     //incorrect
                     Toast.makeText(getApplicationContext(),
