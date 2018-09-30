9a39a314ebf90643e83d5d0686792efb362a4ae7
shubhamgupte01@gmail.com <shubhamgupte01@gmail.com>
2018-09-30 16:49:17 -0400

    Added a way to confirm that passwords match on the screen

--- app/src/main/java/roomies/donationtracker/RegisterActivity.java
+++ app/src/main/java/roomies/donationtracker/RegisterActivity.java
@@ -1,6 +1,7 @@
 package roomies.donationtracker;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +15,33 @@ public class RegisterActivity extends AppCompatActivity {
     Button cancelRegistrationButton;
     EditText user;
     EditText password;
+    EditText confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_screen);
 
-        registerButton = findViewById(R.id.registrationButton);
+        registerButton = findViewById(R.id.signUpButton);
+
+        //for right now, the register button will just check if the passwords match
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password = findViewById(R.id.editPassword);
+                confirmPassword = findViewById(R.id.editPassword2);
+                user = findViewById(R.id.editName);
+                if (password.getText().toString().equals(confirmPassword.getText().toString())) {
+                    Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(i);
+                } else {
+                    AlertDialog fail = new AlertDialog.Builder(RegisterActivity.this).create();
+                    fail.setTitle("Registration");
+                    fail.setMessage("Passwords do not Match!");
+                    fail.show();
+                }
+            }
+        });
 
         cancelRegistrationButton = findViewById(R.id.cancelRegistrationButton);
         cancelRegistrationButton.setOnClickListener(new View.OnClickListener(){
