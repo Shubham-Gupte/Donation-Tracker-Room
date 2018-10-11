f6166f41744d41975eea89cad4eab39d3d8294a3
shubhamgupte01@gmail.com <shubhamgupte01@gmail.com>
2018-10-10 22:18:29 -0400

    This made sure that data is pulled

--- app/src/main/java/roomies/donationtracker/MainActivity.java
+++ app/src/main/java/roomies/donationtracker/MainActivity.java
@@ -1,19 +1,32 @@
 package roomies.donationtracker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
     Button logout;
+    TextView locationText;
 
+    DatabaseReference mainDatabase = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference childReference = mainDatabase.child("locations");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initial_screen);
+
+        locationText = findViewById(R.id.textViewLocation);
         logout = findViewById(R.id.logoutButton);
 
         logout.setOnClickListener(new View.OnClickListener(){
@@ -21,6 +34,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        childReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                long text = dataSnapshot.getChildrenCount();
+                String l = String.valueOf(text);
+                locationText.setText(l);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
