bdd14dac1fe2cc5f7a29475a9b0a18c6915b0881
Carl Henry Roosipuu <carlhenryr@gmail.com>
2018-11-08 18:31:26 -0500

    Fixed

--- app/src/main/java/roomies/donationtracker/activities/LocationDetailsActivity.java
+++ app/src/main/java/roomies/donationtracker/activities/LocationDetailsActivity.java
@@ -3,6 +3,7 @@ package roomies.donationtracker.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
